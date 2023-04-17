25590f2705b1d39bae783822e1f1b94d2ca99526
Simon <saguilar@iastate.edu>
2023-04-17 10:38:31 -0500

    added pages and routing

--- FinalProject/final_project/src/App.js
+++ FinalProject/final_project/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./Home";
+import { SignIn } from "./SignIn";
+import { Pets } from "./Pets";
+import { Checkout } from "./Checkout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/Cart" element={<SignIn />} />
+    <Route path="/Pets" element={<Pets />} />
+    <Route path="/Checkout" element={<Checkout />} />
+  </Routes>
   );
 }
 
