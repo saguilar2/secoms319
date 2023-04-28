606f2d97384043d146ee434df809422745da78d5
Kolby Kucera <kolbykuc@iastate.edu>
2023-04-28 10:28:51 -0500

    video

--- Assignment3/mern_backend/index.js
+++ Assignment3/mern_backend/index.js
@@ -32,3 +32,12 @@ app.get("/", async (req, resp) => {
     console.log(allProducts);
     resp.send(allProducts);
 });
+
+app.get("/:id", async (req, resp) => {
+    const id = req.params.id;
+    const query = { _id: id };
+    const oneProduct = await Product.findOne(query);
+    console.log(oneProduct);
+    resp.send(oneProduct);
+});
+
